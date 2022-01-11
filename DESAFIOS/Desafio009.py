6ea299f22fa4961d978923c4f6157c7c99d83e01
Henry <Henryhosken13@gmail.com>
2022-01-11 15:15:11 -0300

    12

---
+++ DESAFIOS/Desafio009.py
@@ -0,0 +1,13 @@
+Valor= int(input('Digite um valor:'))
+n1= Valor * 1
+n2= Valor * 2
+n3= Valor * 3
+n4= Valor * 4
+n5= Valor * 5
+n6= Valor * 6
+n7= Valor * 7
+n8= Valor * 8
+n9= Valor * 9
+n10= Valor * 10
+
+print ('A Multiplicação do valor digitado é: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10))
